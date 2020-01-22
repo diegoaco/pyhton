a7fa05b15c1d95a3442b1e5e57b93e4b4c0c807a
Diego Carranza Ortiz <59874504+diegoaco@users.noreply.github.com>
2020-01-22 02:29:59 +0000

    Update normal_distribution.py

--- normal_distribution.py
+++ normal_distribution.py
@@ -1,3 +1,5 @@
+''' FIRST PRACTISE WITH MATPLOTLIB'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,13 +7,14 @@ mu, sigma, n = 0, 0.1, 1000  # Define the mean, variance and size of the distrib
 
 s = np.random.normal(mu, sigma, n)  # Creates a random distribution with these parameters
 
-# We check mu and sigma are ok:
+# We check that mu and sigma are ok:
 print(abs(mu - np.mean(s)))
 print(abs(sigma - np.std(s,ddof=1))) # ddof=1 uses an unbaiased estimator
 
-# First we create a histogram (s is in general an array)
+# We create a histogram (s is in general an array):
 count, bins, ignored = plt.hist(s, 30, density='True') # bins = number of intervals
 
+# Now the corresponding normal distribution:
 plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi))*np.exp( - (bins - mu)**2 / (2 * sigma**2)),
-         linewidth = 2, color = 'r')  # We are using bins as our variable x so it's not as smooth
+         linewidth = 2, color = 'r')  # We are using bins as our variable x so it's not that smooth
 plt.show()
