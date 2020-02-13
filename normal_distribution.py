87b4e24f533d123a6143d64fa55dc41bcb167377
Diego Carranza Ortiz <59874504+diegoaco@users.noreply.github.com>
2020-02-13 02:13:04 +0000

    Update normal_distribution.py

--- normal_distribution.py
+++ normal_distribution.py
@@ -3,13 +3,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mu, sigma, n = 0, 0.1, 1000  # Define the mean, variance and size of the distribution
+mu, sigma, n = 0, 1, 1000  # Define the mean, variance and size of the distribution
 
-s = np.random.normal(mu, sigma, n)  # Creates a random distribution with these parameters
+s = np.random.normal(mu, sigma, n)  # Creates a random normal distribution with these parameters
 
 # We check that mu and sigma are ok:
 print(abs(mu - np.mean(s)))
-print(abs(sigma - np.std(s,ddof=1))) # ddof=1 uses an unbaiased estimator
+print(abs(sigma - np.std(s,ddof=1))) # ddof=1 uses an unbiased estimator
 
 # We create a histogram (s is in general an array):
 count, bins, ignored = plt.hist(s, 30, density='True') # bins = number of intervals
